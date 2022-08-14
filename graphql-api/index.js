83789b17b7ab3a1464e184560075862ba6534afc
Navjot Singh Bedi <navjotsinghbedi@gmail.com>
2022-08-13 17:46:16 -0700

    Removed unused dependencies

--- graphql-api/index.js
+++ graphql-api/index.js
@@ -13,23 +13,10 @@ const server = new ApolloServer({
     csrfPrevention: true,
     cache: 'bounded',
     context: ({ req }) => {
-        // Note: This example uses the `req` argument to access headers,
-        // but the arguments received by `context` vary by integration.
-        // This means they vary for Express, Koa, Lambda, etc.
-        //
-        // To find out the correct arguments for a specific integration,
-        // see https://www.apollographql.com/docs/apollo-server/api/apollo-server/#middleware-specific-context-fields
-
-        // Get the user token from the headers.
         var token;
         if(req.headers.authorization) {
             token = req.headers.authorization.trim().split(/\s+/)[1] || '';
-            console.log("token -> " + token)
         }
-        // Try to retrieve a user with the token
-        // const user = getUser(token);
-
-        // Add the user to the context
         return { models, token };
     },
     plugins: [
