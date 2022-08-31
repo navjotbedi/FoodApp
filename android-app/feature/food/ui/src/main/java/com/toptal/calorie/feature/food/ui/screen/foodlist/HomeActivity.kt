9bebe076cd663d68b3c57e47877019bbd09c7bc5
Navjot Singh Bedi <navjotsinghbedi@gmail.com>
2022-08-30 23:49:36 -0700

    Updated food module to compose

--- android-app/feature/food/ui/src/main/java/com/toptal/calorie/feature/food/ui/screen/foodlist/HomeActivity.kt
+++ android-app/feature/food/ui/src/main/java/com/toptal/calorie/feature/food/ui/screen/foodlist/HomeActivity.kt
@@ -3,93 +3,140 @@ package com.toptal.calorie.feature.food.ui.screen.foodlist
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.toptal.calorie.core.android.theme.CalorieAppTheme
 import com.toptal.calorie.core.utils.Constants.FOOD_INTENT
 import com.toptal.calorie.core.utils.Constants.USER_ID_INTENT
 import com.toptal.calorie.feature.food.ui.R
-import com.toptal.calorie.feature.food.ui.databinding.ActivityHomeBinding
 import com.toptal.calorie.feature.food.ui.entity.FoodUIModel
+import com.toptal.calorie.feature.food.ui.entity.HeaderUIModel
 import com.toptal.calorie.feature.food.ui.screen.addfood.AddFoodActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import java.util.*
 
-
 @AndroidEntryPoint
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityHomeBinding
     private val viewModel: HomeViewModel by viewModels()
-    private var fetchFoodTask: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            CalorieAppTheme {
+                Scaffold(
+                    topBar = { TopAppBar(title = { Text(text = stringResource(R.string.food_list_title)) }) },
+                    content = { Surface(modifier = Modifier.fillMaxSize()) { FoodListScreen() } }
+                )
+            }
+        }
+    }
 
-        setupUI()
-        setupListeners()
+    @Composable
+    private fun FoodItemView(foodUIModel: FoodUIModel) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .clickable(enabled = viewModel.isAdmin()) {
+                    if (viewModel.isAdmin()) {
+                        startActivity(Intent(this@HomeActivity, AddFoodActivity::class.java).apply {
+                            putExtra(FOOD_INTENT, Json.encodeToString(FoodUIModel.serializer(), foodUIModel))
+                        })
+                    }
+                }
+                .padding(10.dp)) {
+            Text(text = "Food: ${foodUIModel.name}")
+            Text(text = "Calorie: ${foodUIModel.calorie}")
+        }
+    }
+
+    @Composable
+    private fun HeaderItemView(headerUIModel: HeaderUIModel) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .padding(10.dp)
+        ) {
+            Text(text = headerUIModel.displayDate)
+            Spacer(Modifier.weight(1f))
+            Text(text = "Avg Cal: ${headerUIModel.averageCalorie}")
+        }
+    }
+
+    @Composable
+    private fun FoodListScreen() {
+        val foodList by viewModel.foodList.observeAsState()
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(viewModel.isLoading),
+            onRefresh = { viewModel.saveFoodList() },
+        ) {
+            foodList?.let {
+                LazyColumn {
+                    itemsIndexed(it) { index, foodItem ->
+                        when (foodItem) {
+                            is HeaderUIModel -> HeaderItemView(foodItem)
+                            is FoodUIModel -> FoodItemView(foodItem)
+                        }
+                        if (index < it.lastIndex) {
+                            Divider()
+                        }
+                    }
+                }
+            }
+        }
+
+        addActionButtons()
+    }
+
+    @Composable
+    private fun addActionButtons() {
+        Box(Modifier.padding(15.dp)) {
+            if (viewModel.isAdmin()) {
+                FloatingActionButton(modifier = Modifier.align(Alignment.BottomStart), onClick = {
+                    if (viewModel.isDateFilterApplied) viewModel.fetchFoodList()
+                    else showFilterPopup()
+                }) {
+                    Icon(ImageVector.vectorResource(if (viewModel.isDateFilterApplied) R.drawable.ic_close else R.drawable.ic_filter), "Filter")
+                }
+            }
+            if (!viewModel.isDateFilterApplied) {
+                FloatingActionButton(modifier = Modifier.align(Alignment.BottomEnd), onClick = {
+                    startActivity(Intent(this@HomeActivity, AddFoodActivity::class.java).apply {
+                        putExtra(USER_ID_INTENT, viewModel.userId)
+                    })
+                }) { Icon(Icons.Filled.Add, "Add Food") }
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        loadFoodList()
-    }
-
-    private fun setupListeners() {
-        with(binding) {
-            swipeRefresh.setOnRefreshListener { loadFoodList() }
-            addFoodButton.setOnClickListener {
-                startActivity(Intent(this@HomeActivity, AddFoodActivity::class.java).apply {
-                    putExtra(USER_ID_INTENT, viewModel.userId)
-                })
-            }
-            addDateFilterButton.setOnClickListener {
-                viewModel.isDateFilterApplied.value?.let { idFilterEnable ->
-                    if (idFilterEnable) fetchFoodList()
-                    else showFilterPopup()
-                }
-            }
-            viewModel.isDateFilterApplied.observe(this@HomeActivity) {
-                addDateFilterButton.setImageResource(if (it) R.drawable.ic_close else R.drawable.ic_filter)
-                addFoodButton.visibility = if (it) View.GONE else View.VISIBLE
-            }
-        }
-
-        fetchFoodList()
-    }
-
-    private fun fetchFoodList(startDate: Date? = null, endDate: Date? = null) {
-        fetchFoodTask?.cancel()
-        fetchFoodTask = lifecycleScope.launch {
-            viewModel.fetchFoodList(startDate, endDate)
-                .collectLatest {
-                    binding.swipeRefresh.isRefreshing = false
-                    (binding.foodList.adapter as? FoodListAdapter)?.submitList(it)
-                }
-        }
-    }
-
-    private fun setupUI() {
-        viewModel.storeUserId(intent.getStringExtra(USER_ID_INTENT))
-        with(binding.foodList) {
-            adapter = if (viewModel.isAdmin()) {
-                FoodListAdapter { food ->
-                    startActivity(Intent(this@HomeActivity, AddFoodActivity::class.java).apply {
-                        putExtra(FOOD_INTENT, Json.encodeToString(FoodUIModel.serializer(), food))
-                    })
-                }
-            } else FoodListAdapter()
-            addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
-        }
+        viewModel.fetchFoodList()
+        viewModel.saveFoodList()
     }
 
     private fun showFilterPopup() {
@@ -105,7 +152,7 @@ class HomeActivity : AppCompatActivity() {
             }.time
             DatePickerDialog(
                 this@HomeActivity, { _, y, m, d ->
-                    fetchFoodList(startDate, cal.apply {
+                    viewModel.fetchFoodList(startDate, cal.apply {
                         set(y, m, d)
                         set(Calendar.MINUTE, 0)
                         set(Calendar.HOUR_OF_DAY, 0)
@@ -116,9 +163,4 @@ class HomeActivity : AppCompatActivity() {
             ).apply { setTitle("End Date") }.show()
         }, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).apply { setTitle("Start Date") }.show()
     }
-
-    private fun loadFoodList() {
-        viewModel.saveFoodList()
-    }
-
 }
