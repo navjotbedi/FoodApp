dfd30683522bd0f18e27a5b0ee138e3858aedda9
Navjot Singh Bedi <navjotsinghbedi@gmail.com>
2022-09-04 23:35:18 -0700

    Initiated Unit tests

---
+++ android-app/feature/food/data/src/test/java/com/toptal/calorie/feature/food/data/FoodRemoteDataSourceTest.kt
@@ -0,0 +1,18 @@
+package com.toptal.calorie.feature.food.data
+
+import org.junit.Before
+import org.junit.Test
+
+class FoodRemoteDataSourceTest {
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun `WHEN delete food `() {
+
+    }
+
+}
